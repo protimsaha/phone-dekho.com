74d25ea9e911ce07864e23eb39d2655eda0dbe71
protimsaha <protimsaha750@gmail.com>
2022-03-01 02:18:20 +0600

    detail id got

--- js/app.js
+++ js/app.js
@@ -12,27 +12,29 @@ const loadPhones = () => {
 
     toggleSpinner('block')
 
-    const searchBox = document.getElementById('search-box')
-    const searchText = searchBox.value;
-
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showResult(data.data))
+    let searchBox = document.getElementById('search-box')
+    let searchText = searchBox.value;
+    if (document.getElementById('search-box').value == '') {
+        toggleMessage('block');
+        toggleSpinner('none');
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => showResult(data.data))
+    }
+    searchBox.value = ''
 }
 
 // show search result
 const showResult = phones => {
-    console.log(phones)
 
+    // show field
     const showResultDisplay = document.getElementById('show-result');
-    showResult.textContent = '';
+    showResultDisplay.textContent = '';
 
     if (phones.length == 0) {
-        toggleMessage('block');
-        toggleSpinner('none');
-    }
-    else if (document.getElementById('search-box').value == '') {
         toggleMessage('block');
         toggleSpinner('none');
     }
@@ -44,17 +46,26 @@ const showResult = phones => {
             const div = document.createElement('div');
             div.innerHTML = `
             <div class="col">
-            <div class="card h-100">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card h-100 ">
+            <img class="w-75 mx-auto" src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
             <h4 class="card-title">Model name-${phone.phone_name}</h4>                   
             <h5>Brand-${phone.brand}</h5>
+            <button onclick="loadDetail('${phone.slug}')" class="btn btn-info  end-0">Detail</button>
             </div>
             </div>
             </div>
             `
             showResultDisplay.appendChild(div)
         })
-        toggleSpinner('none')
+
+        toggleSpinner('none');
+        toggleMessage('none');
     }
+}
+const loadDetail = (phoneId) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
