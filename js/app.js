a304c4bb37b90e6f5129922d2007e94069a79562
protimsaha <protimsaha750@gmail.com>
2022-03-01 00:32:32 +0600

    search result show

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+// spinnerShow
+const toggleSpinner = display => {
+    document.getElementById('spinner').style.display = display
+}
+// no phone found
+const toggleMessage = displayMessage => {
+    document.getElementById('result-null').style.display = displayMessage
+}
+
 // search phones
 const loadPhones = () => {
+
+    toggleSpinner('block')
 
     const searchBox = document.getElementById('search-box')
     const searchText = searchBox.value;
@@ -13,4 +24,37 @@ const loadPhones = () => {
 // show search result
 const showResult = phones => {
     console.log(phones)
+
+    const showResultDisplay = document.getElementById('show-result');
+    showResult.textContent = '';
+
+    if (phones.length == 0) {
+        toggleMessage('block');
+        toggleSpinner('none');
+    }
+    else if (document.getElementById('search-box').value == '') {
+        toggleMessage('block');
+        toggleSpinner('none');
+    }
+    else {
+
+        phones?.forEach(phone => {
+            console.log(phone)
+
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="col">
+            <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h4 class="card-title">Model name-${phone.phone_name}</h4>                   
+            <h5>Brand-${phone.brand}</h5>
+            </div>
+            </div>
+            </div>
+            `
+            showResultDisplay.appendChild(div)
+        })
+        toggleSpinner('none')
+    }
 }
